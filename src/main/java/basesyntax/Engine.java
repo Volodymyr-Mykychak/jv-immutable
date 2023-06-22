d70d6b2239d1d010565cafe4a1ca97cf75e5d650
Volodymyr Mykychak <leadervlo.d@gmail.com>
2023-06-22 17:42:45 +0300

    test commit

---
+++ src/main/java/basesyntax/Engine.java
@@ -0,0 +1,16 @@
+package basesyntax;
+
+public class Engine {
+    private int horsePower;
+    private String manufacturer;
+
+    //implement this class
+
+    @Override
+    public String toString() {
+        return "Engine{"
+            + "horsePower=" + horsePower
+            + ", manufacturer='" + manufacturer + '\''
+            + '}';
+    }
+}
--- src/test/java/core/basesyntax/CarTest.java
+++ src/test/java/core/basesyntax/CarTest.java
@@ -1,14 +1,15 @@
 package core.basesyntax;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 public class CarTest {
     private Engine testEngine;
